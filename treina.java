4dd054e4f00d54794064d4b80f8dd040f52d35d5
jeffersontreinarecife <jefferson.treinarecife@gmail.com>
2026-01-14 23:33:26 +0000

    Criação do arquivo

--- treina.java
+++ treina.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
 
+public class treina {
+
+    public static void main (String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite um valor maior que 10: ");
+        int valor = scan.nextInt();
+
+        if(valor <10){
+
+            System.out.println("Valor menor que 10");
+
+        }else{
+
+            System.out.println("Valor digitado: " + valor);
+        }
+
+    }
+}
